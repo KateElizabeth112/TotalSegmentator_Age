a2407ef7c275d409f8af71f04639024e720820f7
Kate Cevora <kate.wilkinson@illumr.com>
2023-11-16 16:00:28 +0000

    Removed duplicate line

--- createDatasetInfo.py
+++ createDatasetInfo.py
@@ -5,6 +5,8 @@ import os
 import pandas as pd
 import pickle as pkl
 import argparse
+import numpy as np
+
 
 # argparse
 parser = argparse.ArgumentParser(description="Just an example",  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -21,7 +23,7 @@ else:
 
 images_folder = os.path.join(root_folder, "nnUNet_raw/Dataset300_Full/imagesTr")
 
-def main():
+def create():
 
     meta = pd.read_csv(os.path.join(root_folder, "meta.csv"), sep=";")
     patients_all = meta["image_id"].values
@@ -45,7 +47,6 @@ def main():
 
         age.append(age_all[patients_all == "s" + id])
         genders.append(genders_all[patients_all == "s" + id])
-        age.append(age_all[patients_all == "s" + id])
         institute.append(institute_all[patients_all == "s" + id])
         study_type.append(study_type_all[patients_all == "s" + id])
 
@@ -59,6 +60,24 @@ def main():
     f = open(os.path.join(root_folder, "info.pkl"), "wb")
     pkl.dump(info, f)
     f.close()
+
+
+def explore():
+    f = open(os.path.join(root_folder, "info.pkl"), "rb")
+    info = pkl.load(f)
+    f.close()
+
+    age = info["age"]
+    age = np.squeeze(np.array(age))
+
+    plt.clf()
+    plt.hist(age)
+    plt.show()
+
+
+def main():
+    create()
+    #explore()
 
 
 if __name__ == "__main__":
