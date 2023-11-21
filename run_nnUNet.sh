578eb3501691c1dc70b17f80535ee5dbdddffce3
Kate Cevora <kate.wilkinson@illumr.com>
2023-11-21 11:17:14 +0000

    Updated to 902

--- run_nnUNet.sh
+++ run_nnUNet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -l walltime=48:00:00
 #PBS -l select=1:ncpus=18:mem=100gb:ngpus=1:gpu_type=RTX6000
-#PBS -N nnUNet_TS_age_901
+#PBS -N nnUNet_TS_age_902
 
 cd ${PBS_O_WORKDIR}
 
@@ -14,7 +14,7 @@ python -c "import torch;print(torch.cuda.is_available())"
 
 # Set environment variables
 ROOT_DIR='/rds/general/user/kc2322/projects/cevora_phd/live/TotalSegmentator/'
-DATASET='Dataset901_Age4'
+DATASET='Dataset902_Age4'
 TASK=${DATASET:7:3}
 
 export nnUNet_raw=$ROOT_DIR"nnUNet_raw"
