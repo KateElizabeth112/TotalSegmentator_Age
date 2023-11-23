5bc5aa9ab966c5c99e015429d5b8dee07fe4a5e5
Kate Cevora <kate.wilkinson@illumr.com>
2023-11-23 11:07:57 +0000

    Fixed dataset name

--- predict_nnUNet.sh
+++ predict_nnUNet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -l walltime=10:00:00
 #PBS -l select=1:ncpus=15:mem=120gb:ngpus=1:gpu_type=RTX6000
-#PBS -N nnUNet_TS_predict_500
+#PBS -N TS_predict_500
 
 cd ${PBS_O_WORKDIR}
 
@@ -15,7 +15,7 @@ python -c "import torch;print(torch.cuda.is_available())"
 # Set environment variables
 ROOT_DIR='/rds/general/user/kc2322/projects/cevora_phd/live/TotalSegmentator/'
 
-datasets=("Dataset500_Fold0" "Dataset501_Fold0" "Dataset502_Fold0")
+datasets=("Dataset500_Age0" "Dataset501_Age0" "Dataset502_Age0")
 
 export nnUNet_raw=$ROOT_DIR"nnUNet_raw"
 export nnUNet_preprocessed=$ROOT_DIR"nnUNet_preprocessed"
